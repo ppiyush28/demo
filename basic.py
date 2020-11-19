ee88aee389ba06e25b7d19890adaeac2c8b027d5
ppiyush28 <piyushdpande@gmail.com>
2020-11-19 18:12:19 +0530

    python

---
+++ basic.py
@@ -0,0 +1,48 @@
+# Singleline comment
+
+'''
+Multiline comment
+'''
+
+"""
+Multiline comment
+"""
+
+# integer value
+#a= 10
+a =10.5
+print('type(a) is ',type(a))
+print('Value of a is',a)
+
+print('========================')
+
+b= 10.56
+print('type(a) is ',type(b))
+print('Value of a is',b)
+
+print('========================')
+
+b= 10.56
+print('type(b) is ',type(b))
+print('Value of b is',b)
+
+print('========================')
+
+c= 'c'
+print('type(c) is ',type(c))
+print('Value of c is',c)
+
+print('========================')
+
+d= "String"
+print('type(d) is ',type(d))
+print('Value of d is',d)
+
+print('========================')
+
+e= 50
+print ('type (e) is', type(e))
+print ('value of e is', e)
+
+
+
